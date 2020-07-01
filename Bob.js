48263cb0478ef43363dd35c5568608985095b877
ShineyViolist <65990530+ShineyViolist@users.noreply.github.com>
2020-07-01 13:44:35 -0400

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -2,8 +2,8 @@ class Bob{
     constructor(x,y){
         var options = {
             isStatic:false,
-            restitution:0.3,
-            friction:0.5,
+            restitution:1.0,
+            friction:0.0,
             density:1.2
         }
         this.body = Bodies.circle(x,y,20,options);
